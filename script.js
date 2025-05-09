02f749be5a74b2efa966bdddf9ffc29e10dd67b9
JKHZii <prainsofakai@gmail.com>
2025-05-08 22:49:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ for (let i = 0; i < 20; i++) {
 setInterval(createFlower, 300);
 
 // Efecto máquina de escribir
-const text = "Que eres la mejor";
+const text = "QUE ERES LA MEJOR";
 const animatedText = document.getElementById('animatedText');
 let i = 0;
 
